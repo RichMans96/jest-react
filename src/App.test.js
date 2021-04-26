7f5bc3a76ffe17e01558804d4073944115ae4f37
RichMans96 <richard.mansworth@hotmail.co.uk>
2021-04-26 17:01:25 +0100

    added a beforeEach to make tests more DRY and refactored tests to be more concise

--- src/App.test.js
+++ src/App.test.js
@@ -2,27 +2,28 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import Main from './Main';
 
 describe('Tests for main component', () => {
+  let getByTestId;
+
+  beforeEach(() => {
+    const component = render(<Main />);
+    getByTestId = component.getByTestId;
+  });
+
   test('it matches the snapshot', () => {
     const tree = render(<Main />);
 
     expect(tree).toMatchSnapshot();
   });
 
-  test('it displays 0', () => {
-    let count;
-
-    render(<Main />);
-    count = screen.getByTestId('count-display');
+  test('it displays the intial state of 0', () => {
+    const count = getByTestId('count-display');
 
     expect(count.innerHTML).toBe('0');
   });
 
   test('count displays 1 when increment button is clicked', () => {
-    let count;
-
-    render(<Main />);
-    count = screen.getByTestId('count-display');
-    const increment = screen.getByTestId('increment-button');
+    const count = getByTestId('count-display');
+    const increment = getByTestId('increment-button');
 
     fireEvent.click(increment);
 
@@ -30,11 +31,8 @@ describe('Tests for main component', () => {
   });
 
   test('count displays -1 when decrement button is clicked', () => {
-    let count;
-
-    render(<Main />);
-    count = screen.getByTestId('count-display');
-    const decrement = screen.getByTestId('decrement-button');
+    const count = getByTestId('count-display');
+    const decrement = getByTestId('decrement-button');
 
     fireEvent.click(decrement);
 
